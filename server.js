9978d6a42fb0a4567c694b33778674d7847ba495
Utkarshende <utkarshapd1403@gmail.com>
2026-05-06 15:56:57 +0530

    chore: convert project to ES Modules and add express-async-handler

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import app from './src/app.js';
+import connectDB from './src/config/db.js';
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/errorMiddleware.js';
+
+dotenv.config();
 
 const app = express();
 
-app.use(helmet()); 
-app.use(cors());   
-app.use(morgan('dev')); 
-app.use(express.json()); 
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Backend Engine is Purring' });
 });
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(errorHandler);
 
-module.exports = app;
+export default app;
